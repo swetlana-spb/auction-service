220f69174d494ef41572ff88467e479d762a2c0b
Svetlana K <swetlana-spb@mail.ru>
2023-04-10 17:51:22 +0400

    add middleware

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,9 +1,14 @@
 import { v4 as uuid } from 'uuid';
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
 import { ddbDocClient } from "../libs/ddbDocClient";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
 
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
   const now = new Date();
 
   const auction = {
@@ -13,10 +18,16 @@ async function createAuction(event, context) {
     createdAt: now.toISOString(),
   };
 
-  await ddbDocClient.send(new PutCommand({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction,
-  }));
+  try {
+    await ddbDocClient.send(new PutCommand({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }));
+  }
+  catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
@@ -24,6 +35,7 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
-
-
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
